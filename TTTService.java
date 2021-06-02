81167eb537c2b2eb6a8ab8640be55fbf9bae97f8
SoniaSibele <76952338+SoniaSibele@users.noreply.github.com>
2021-06-02 09:39:19 -0100

    Delete TTTService.java

--- TTTService.java
+++
@@ -1,9 +0,0 @@
-
-import java.rmi.*;
-public interface TTTService extends Remote {
-    String currentBoard() throws RemoteException;
-    boolean play(int row, int column, int player) throws RemoteException;
-    int checkWinner() throws RemoteException;
-    void restart() throws RemoteException;
-    void removeLastPlay() throws RemoteException;
-}
